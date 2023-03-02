2e73de4ec466e691bac62bf7bb4f0c6408bca7b6
Tobias Krauss <tobias.tk.krauss@gmail.com>
2023-03-02 17:52:35 +0100

    add basic styling

--- src/pages/TreasureDetailsPage.js
+++ src/pages/TreasureDetailsPage.js
@@ -4,6 +4,8 @@ import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import ItemCard from "../components/ItemCard";
 import AddItem from "../components/AddItem";
+import trashIcon from "../images/trash.png"
+import editIcon from "../images/edit.png"
 
 const API_URL = "http://localhost:5005"
 
@@ -38,15 +40,30 @@ function TreasureDetailsPage() {
     
 
     return (
-        <div className="ProjectDetails">
+        <div className="treasure-details">
+            <div class="overlay"></div>
             {treasure && (
                 <>
-                    <img src={treasure.imageUrl} alt="treasure" style={{ width: 400 }} />
+                <div className="details-icon">
+                    <div className="img-edit">
+                <Link to={`/treasure/edit/${treasureId}`}>
+                  <img src={editIcon} alt="trash" style={{ height: 25, marginRight: 5, hover: "red" }}  />
+                  </Link>
+                  </div>
+                  <div className="img-delete">
+                  <Link to="/treasure">
+                  <img src={trashIcon} alt="trash" style={{ height: 22, marginLeft: 3 }} />
+                  </Link>
+                  </div>
+                  </div>
+                  <div className="details-details">
+                    <img src={treasure.imageUrl} alt="treasure" style={{ width: 400, borderRadius: 5 }} />
                     <h1>{treasure.title}</h1>
                     <p>{treasure.description}</p>
                     <p>{treasure.owner}</p>
                     <p>{treasure.street}, {treasure.zipcode} {treasure.city}</p>
-                </>
+                    </div>
+                    </>
             )}
 
             {treasure && treasure.items.map(item => {
@@ -60,9 +77,6 @@ function TreasureDetailsPage() {
 
             <Link to="/treasure">
                 <button>Back to all Treasure</button>
-            </Link>
-            <Link to={`/treasure/edit/${treasureId}`}>
-                <button>Edit Treasure</button>
             </Link>
         </div>
     );
