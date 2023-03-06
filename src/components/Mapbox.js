791ccef63e7df05dd7927bbbf6248adc48cfb6de
Tobias Krauss <tobias.tk.krauss@gmail.com>
2023-03-06 14:03:53 +0100

    add marker to mapbox

--- src/components/Mapbox.js
+++ src/components/Mapbox.js
@@ -5,56 +5,47 @@ import { useEffect } from "react";
 
 function Mapbox() {
 
+    function initializeMap() {
+        const map = createMap();
+    }
+
     function createMap() {
         mapboxgl.accessToken = "pk.eyJ1IjoidG9ia3JhdXNzIiwiYSI6ImNsZXdrdW5jYzBmdG8zdmtjNzE3MmlmemMifQ.DS6DX6bmuHXqFRqJOW-f7A"
-        const map = new mapboxgl.Map({
+        let map = new mapboxgl.Map({
             container: "map",
-            style: "mapbox://styles/mapbox/streets-v12",
+            style: "mapbox://styles/mapbox/outdoors-v12",
             center: [13.466976165771484, 52.519065856933594],
             zoom: 11,
             projection: "globe"
         })
 
         const nav = new mapboxgl.NavigationControl()
-map.addControl(nav, "top-left")
+        map.addControl(nav, "top-left")
 
+        new mapboxgl.Marker({
+            color: "red",
+            draggable: false
+        })
+        .setLngLat([13.466976165771484, 52.519065856933594])
+        .addTo(map)
     
 
-new mapboxgl.Marker({
-    color: "red",
-    draggable: true
-})
-.setLngLat([13.75,52.9])
-.addTo(map)
-.on("dragend", event => console.log(event.target._lngLat))
-
-function addMarker(event){
-    new mapboxgl.Marker({
-        color: "blue"
-    })
-    .setLngLat(event._lngLat)
-    .addTo(map)
-
-
-map.on("click", addMarker)
-
-}
+        return map;
     }
 
 
-    
 
-    useEffect(() => {
-        createMap()
-    }, [])
+useEffect(() => {
+    initializeMap()
+}, [])
 
 
-    return (
+return (
 
-        <div id="map" style={{width: "70vw", height: "80vh"}}>
-        </div>
+    <div id="map" style={{ width: "70vw", height: "80vh" }}>
+    </div>
 
-    );
+);
 
 }
 
